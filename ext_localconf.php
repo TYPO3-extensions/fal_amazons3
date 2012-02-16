c22b0adabb6113381219b7dd6cb37c21a2cecd18
Benjamin Mack <benni@typo3.org>
2012-02-16 17:14:53 +0100

    [BUGFIX] Added flxform configuration and corrected inclusion

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,5 +4,8 @@ if (!defined('TYPO3_MODE')) {
 }
 
 	// register additional driver
-$TYPO3_CONF_VARS['SYS']['fal']['registeredDrivers']['AmazonS3'] = 'Tx_FalAmazonS3_Driver_AmazonS3Driver',
-
+$TYPO3_CONF_VARS['SYS']['fal']['registeredDrivers']['AmazonS3'] = array(
+	'class' => 'Tx_FalAmazonS3_Driver_AmazonS3Driver',
+	'label' => 'Amazon S3',
+	'flexFormDS' => 'EXT:fal_amazons3/Configuration/AmazonS3DriverFlexForm.xml'
+);
